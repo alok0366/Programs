214e9c663106d1e8514beaae504e6d2350f38d61
alok0366 <52827760+alok0366@users.noreply.github.com>
2021-04-08 19:18:39 +0530

    Update Text_To_Speech.py

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -6,6 +6,67 @@ try:
         print("ENTER THE TEXT WHICH DO U WANT TO CONVERT:")
         text=input()
         print("NOW ENTER YOUR PREFERRED LANGUAGE(MUST BE IN ABBREVIATED FORM):")
+        lgm={
+        'af': 'Afrikaans',
+        'ar': 'Arabic',
+        'bn': 'Bengali',
+        'bs': 'Bosnian',
+        'ca': 'Catalan',
+        'cs': 'Czech',
+        'cy': 'Welsh',
+        'da': 'Danish',
+        'de': 'German',
+        'el': 'Greek',
+        'en': 'English',
+        'eo': 'Esperanto',
+        'es': 'Spanish',
+        'et': 'Estonian',
+        'fi': 'Finnish',
+        'fr': 'French',
+        'gu': 'Gujarati',
+        'hi': 'Hindi',
+        'hr': 'Croatian',
+        'hu': 'Hungarian',
+        'hy': 'Armenian',
+        'id': 'Indonesian',
+        'is': 'Icelandic',
+        'it': 'Italian',
+        'ja': 'Japanese',
+        'jw': 'Javanese',
+        'km': 'Khmer',
+        'kn': 'Kannada',
+        'ko': 'Korean',
+        'la': 'Latin',
+        'lv': 'Latvian',
+        'mk': 'Macedonian',
+        'ml': 'Malayalam',
+        'mr': 'Marathi',
+        'my': 'Myanmar (Burmese)',
+        'ne': 'Nepali',
+        'nl': 'Dutch',
+        'no': 'Norwegian',
+        'pl': 'Polish',
+        'pt': 'Portuguese',
+        'ro': 'Romanian',
+        'ru': 'Russian',
+        'si': 'Sinhala',
+        'sk': 'Slovak',
+        'sq': 'Albanian',
+        'sr': 'Serbian',
+        'su': 'Sundanese',
+        'sv': 'Swedish',
+        'sw': 'Swahili',
+        'ta': 'Tamil',
+        'te': 'Telugu',
+        'th': 'Thai',
+        'tl': 'Filipino',
+        'tr': 'Turkish',
+        'uk': 'Ukrainian',
+        'ur': 'Urdu',
+        'vi': 'Vietnamese',
+        'zh-CN': 'Chinese'}
+        for i in lgm:
+            print(i,lgm[i])
         lang=input()
         speech=gTTS(text=text,lang=lang,slow=False)
         speech.save("test.mp3")
@@ -14,7 +75,7 @@ try:
         q=input()
         if q=="NO":
             break
-except:
-    pass
+except EOFError as e:
+    print(e)
         
         
